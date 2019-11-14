2be7b3a98645b45f1b0cc186ed07dd964587245a
Helen Lin <30956318+hlin98@users.noreply.github.com>
2019-11-14 15:42:03 -0500

    auto increment review id

--- reviews_db.php
+++ reviews_db.php
@@ -6,7 +6,7 @@
    $review_text = $_POST['review_text'];
    $rating = $_POST['rating'];
 
-   $query = "INSERT INTO review(review_id, restaurant_name, review_text, student_email, rating) VALUES ('544354', 'Aberdeen Barn', '$review_text', 'hl5ec@virginia.edu', '$rating');";
+   $query = "INSERT INTO review(restaurant_name, review_text, student_email, rating) VALUES ('Aberdeen Barn', '$review_text', 'hl5ec@virginia.edu', '$rating');";
    $statement = $db->prepare($query);
    $statement->execute();     // if the statement is successfully executed, execute() returns true
                               // false otherwise 
