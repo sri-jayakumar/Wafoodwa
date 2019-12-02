fc9dd2e69888beda5ef8a5dffd89e76d8f82352a
Helen Lin <30956318+hlin98@users.noreply.github.com>
2019-12-02 11:50:21 -0500

    added nav bar to reviews page

--- reviews.php
+++ reviews.php
@@ -26,9 +26,29 @@ $action = "view_friend";        // default action
 h4{
 	margin-top: 4%;
 }
+
+
 </style>
 </head>
 <body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">WAFOODWA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="profile.php">Profile</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 	<div class="container">
 		<b><font size="7"
 		<?php foreach ($onerest as $restaurant): ?>
