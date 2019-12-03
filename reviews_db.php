fa79b8b602247b8f80698e973aaba10f56f63c15
Sri Jay <sj4zm@virginia.edu>
2019-12-03 17:53:16 -0500

    added trigger to reviews

--- reviews_db.php
+++ reviews_db.php
@@ -12,20 +12,12 @@
    $email = $_SESSION['email']; 
 
    $query = "INSERT INTO review(restaurant_name, review_text, student_email, username, rating) VALUES ('$restaurant_name', '$review_text', '$email','$username','$rating');";
-   $query2 = "INSERT INTO writes(username, review_id) VALUES ('$username', LAST_INSERT_ID());";
-   $query3 = "INSERT INTO Written_for(review_id, restaurant_name) VALUES (LAST_INSERT_ID(), '".$restaurant_name."');";
 
    $statement = $db->prepare($query);
-   $statement2 = $db->prepare($query2);
-   $statement3 = $db->prepare($query3);
 
    $statement->execute();
-   $statement2->execute();
-   $statement3->execute();
 
    $statement->closeCursor();
-   $statement2->closeCursor();
-   $statement3->closeCursor();
 
    $encode = urlencode($restaurant_name);
    header("Location: ./reviews.php?" . $encode);
